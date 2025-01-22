ae50d1508462b1d9a7e7d8eecbc5de21745fe23a
Mihkel <mihkel.lepberg@voco.ee>
2025-01-22 12:08:10 +0200

    apache2

--- apache_paigaldus.sh
+++ apache_paigaldus.sh
@@ -8,13 +8,11 @@ APACHE2=$(dpkg-query -W -f='${status}' apache2 2>/dev/null | grep -c 'ok install
 # if apache 2 is not installed
 if [ $APACHE2 -eq 0 ]; then
 	# installs apache2 and shows status
-	echo -e "\nInstallingpackage apache2\n"
+	echo -e "\nInstalling package apache2\n"
 	apt install apache2 -y
 	echo -e "\napache2 package has been installed\n"
-	systemctl status apache2
 # if apache is installed
 elif [ $APACHE2 -eq 1 ]; then
 	# shows apache2 status
 	echo -e "\napache2 package has already been installed\n"
-	systemctl status apache2
 fi
