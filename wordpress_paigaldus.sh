d3fd08dfd59530a3ede44701357f772dc4e4c4b1
Mihkel <mihkel.lepberg@voco.ee>
2025-01-28 14:59:08 +0200

    typo

--- wordpress_paigaldus.sh
+++ wordpress_paigaldus.sh
@@ -9,7 +9,9 @@ WP=$(dpkg-query -W -f='${status}' wordpress 2>/dev/null | grep -c 'ok installed'
 if [ $WP -eq 0 ]; then
 	# installs wordpress and shows status
 	echo -e "\nInstalling package wordpress\n"
-	apt install wordpress -y
+	wget https://wordpress.org/latest.zip
+	unzip latest.zip -d /var/www/html/
+	rm latest.zip
 	echo -e "\nwordpress package has been installed\n"
 # if wordpress is installed
 elif [ $WP -eq 1 ]; then
@@ -18,12 +20,12 @@ elif [ $WP -eq 1 ]; then
 fi
 
 # if the config file does not exist copies it from the default folder
-if [ ! -e "/etc/wordpress/config-default.php" ]; then
-	cp /usr/share/wordpress/wp-config-sample.php /etc/wordpress/config-default.php
+if [ ! -e "/var/www/html/wordpress/wp-config.php" ]; then
+	cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 	# replaces the database settings
-	sed -i "s/database_name_here/wordpress/g" /etc/wordpress/config-default.php
-	sed -i "s/username_here/wpuser/g" /etc/wordpress/config-default.php
-	sed -i "s/password_here/qwerty/g" /etc/wordpress/config-default.php
+	sed -i "s/database_name_here/wpdatabse/g" /var/www/html/wordpress/wp-config.php
+	sed -i "s/username_here/wpuser/g" /var/www/html/wordpress/wp-config.php
+	sed -i "s/password_here/qwerty/g" /var/www/html/wordpress/wp-config.php
 	echo -e "\nconfig file created\n"
 else
 	echo -e "\nconfig file exists\n"
